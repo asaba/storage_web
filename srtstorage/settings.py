e75d416594329db654abe664ee25201303c8244b
asaba <asaba@oa-cagliari.inaf.it>
2016-12-01 17:40:25 +0100

    Fixing admin suit pages

--- srtstorage/settings.py
+++ srtstorage/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     "captcha",
     'dajaxice',
     "storage",
-    #'suit',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
